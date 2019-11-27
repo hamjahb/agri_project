459dcd9e2948c575bffa03cc275e9e3500316b8c
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-11-27 16:02:44 +0300

    qr code sorta working

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,8 +1,30 @@
 class LogsController < ApplicationController
+  require 'rqrcode'
+
   def index
     @farm = Farm.find(params[:farm_id])
     @plot = Plot.find(params[:plot_id])
     @logs = @plot.logs.all
+
+
+    qrcode = RQRCode::QRCode.new("http://localhost:3000/farms/1/")
+
+    # NOTE: showing with default options specified explicitly
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: 'black',
+      file: nil,
+      fill: 'white',
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 120
+    )
+    
+    IO.write("logs.png", png.to_s)
+
   end
 
   def new
